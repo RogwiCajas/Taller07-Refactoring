df3c164e38b1c963f5e8a9f8873e3f05be97f9fc
Jeffrey Prado <55856634+JeffP11@users.noreply.github.com>
2020-08-13 20:48:58 -0500

    Feature Envy en clase Materia y Estudiante

--- src/modelos/Materia.java
+++ src/modelos/Materia.java
@@ -8,4 +8,25 @@ public class Materia {
     public double notaFinal;
     public double notaTotal;
     
+    
+    //Calcula y devuelve la nota inicial contando examen, deberes, lecciones y talleres. El teorico y el practico se calcula por parcial.
+    private double CalcularNotaInicial(double nexamen,double ndeberes, double nlecciones, double ntalleres){
+        double notaTeorico=(nexamen+ndeberes+nlecciones)*0.80;
+        double notaPractico=(ntalleres)*0.20;
+        return notaTeorico+notaPractico;
+    }
+    
+    //Calcula y devuelve la nota final contando examen, deberes, lecciones y talleres. El teorico y el practico se calcula por parcial.
+    
+    private double CalcularNotaFinal(double nexamen,double ndeberes, double nlecciones, double ntalleres){
+        double notaTeorico=(nexamen+ndeberes+nlecciones)*0.80;
+        double notaPractico=(ntalleres)*0.20;
+        return notaTeorico+notaPractico;
+    }
+    
+    //Calcula y devuelve la nota inicial contando examen, deberes, lecciones y talleres. Esta nota es solo el promedio de las dos calificaciones anteriores.
+    private double CalcularNotaTotal(double notaI, double notaF){
+        return notaI+notaF/2;
+    }
+    
 }
